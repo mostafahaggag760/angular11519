b996e54fb66a3c401a211a9f118c19d36f187e5e
mostafahaggag760 <mostafahaggag760@gmail.com>
2019-05-21 09:04:45 +0200

    routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+import { EmployeeListComponent } from './employee-list/employee-list.component';
+import { DepartmentListComponent } from './department-list/department-list.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  {path: 'department',component: DepartmentListComponent},
+  {path:'employee',component: EmployeeListComponent}
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
+export const routingComponents = [DepartmentListComponent,EmployeeListComponent]
